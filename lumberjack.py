6710e7613bfe6d15e68543f21b039caac1ff5a17
Greg Whitaker <gregwhit2@gmail.com>
2016-04-23 22:13:25 +0200

    initial commit

---
+++ lumberjack.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+
+if name == "Greg":
+    print("{} is a rockstar and he's OK!".format(name))
+else:
+    print("{} sleeps all night and {} works all day!".format(name, name))
